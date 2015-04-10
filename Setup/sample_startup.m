10de108caff9825bbfe2c72dc4843f096295efec
arwoll <arthurwoll@gmail.com>
2015-04-10 10:45:28 -0400

    Added a sample startup file for Utils -- see sample_startup.m

---
+++ Setup/sample_startup.m
@@ -0,0 +1,10 @@
+% startup.m
+AW_MATLAB_BASE = '/home/aw30/Matlab/';
+
+% GID persona
+addpath([AW_MATLAB_BASE 'Matlab_Xray_Utils/Setup:'])
+gid_setup
+
+% Setup Plotting defaults (from Plotting_General)
+plotting_defaults
+
