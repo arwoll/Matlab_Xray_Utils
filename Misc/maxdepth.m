6ecb2285b182543d01fa38ae409d0c6a605c8051
arwoll <arthurwoll@gmail.com>
2015-04-28 11:25:18 -0400

    Add cxrf tool maxdepth to Misc

---
+++ Misc/maxdepth.m
@@ -0,0 +1,13 @@
+function y = maxdepth(wd, dia, theta)
+%function y = maxdepth(wd, dia, theta)
+%
+% Returns the maximum scan depth in confocal XRF, given the working distance (wd),
+% polycapillary diameter (dia), and the angle between the surface and polycapillary
+% (in degrees)
+r = dia/2.0;
+phi = atan(r/wd);
+surface_to_corner = wd/cos(phi); % = sqrt(r^2 + wd^2)
+th = theta*pi/180.0;
+elev = th-phi;
+
+y = surface_to_corner*sin(elev);
